d3acdda1ae0bcee02c26e88992a4695c171bd365
didier1969 <fccourchapoix@gmail.com>
2019-05-04 10:06:23 +0200

    Update __init__.py

--- dockerfiles/Odoo/odoo11-custom-modules/webkul_neo4j_automated/__init__.py
+++ dockerfiles/Odoo/odoo11-custom-modules/webkul_neo4j_automated/__init__.py
@@ -21,7 +21,7 @@ def pre_init_check(cr):
     from odoo.exceptions import Warning
     version_info = common.exp_version()
     server_serie = version_info.get('server_serie')
-    if server_serie != '11.0':
+    if server_serie != '12.0':
         raise Warning(
-            'Module support Odoo series 11.0 found {}.'.format(server_serie))
+            'Module support Odoo series 12.0 found {}.'.format(server_serie))
     return True
